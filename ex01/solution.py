93db2e3234af851f97dd945f0651a030e3cb4d9f
Andre Davis <65955283+andredavis-dev@users.noreply.github.com>
2026-05-31 15:00:40 -0400

    Add user input for name and age with message loop

--- ex01/solution.py
+++ ex01/solution.py
@@ -1,1 +1,11 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
 
+year_turn_100 = (2026 - age) + 100
+
+print(name + ", will turn 100 in the year " + str(year_turn_100))
+
+num = int(input("Pick a number: "))
+
+for i in range(num):
+    print("Message copied")
